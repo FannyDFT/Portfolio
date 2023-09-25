f564a47ca6f390b2bc01bd3e6c9e726329a1cf3b
Fanny D'Erfurth <fannyd.erfurth@yahoo.fr>
2023-09-25 14:00:31 +0200

    Added datas arrays and pictures

---
+++ portfolio/src/datas/datas.ts
@@ -0,0 +1,169 @@
+export const technicalSkills = [
+  { id: 1, name: "HTML" },
+  { id: 2, name: "CSS" },
+  { id: 3, name: "JavaScript" },
+  { id: 4, name: "TypeScript" },
+  { id: 5, name: "Python" },
+  { id: 6, name: "React" },
+  { id: 7, name: "Next.js" },
+  { id: 8, name: "TailwindCSS" },
+  { id: 9, name: "Sass" },
+  { id: 10, name: "NeutralinoJS" },
+  { id: 11, name: "Node.js" },
+  { id: 12, name: "Express" },
+  { id: 13, name: "MySQL" },
+  { id: 14, name: "PostgreSQL" },
+  { id: 15, name: "Prisma" },
+  { id: 16, name: "Docker" },
+  { id: 17, name: "Figma" },
+  { id: 18, name: "Git/GitHub" },
+];
+
+export const projects = [
+  {
+    id: 1,
+    image: "../../../../assets/images/bionbe/accueil.png",
+    title: "Bionbe",
+    subtitle: "Projet de Stage",
+  },
+  {
+    id: 2,
+    image: "",
+    title: "Aelys",
+    subtitle: "Projet de Stage",
+  },
+  {
+    id: 3,
+    image: "",
+    title: "Bijoux de Soins",
+    subtitle: "Projet autonome",
+  },
+  {
+    id: 4,
+    image: "",
+    title: "Externatic",
+    subtitle: "Projet de Formation en Groupe",
+  },
+  {
+    id: 5,
+    image: "",
+    title: "EcoTripr",
+    subtitle: "Projet de Formation en Groupe",
+  },
+  {
+    id: 6,
+    image: "",
+    title: "FleetGo",
+    subtitle: "Projet de Formation en Groupe",
+  },
+  {
+    id: 7,
+    image: "",
+    title: "WildFit",
+    subtitle: "Projet de Formation en Groupe",
+  },
+  {
+    id: 8,
+    image: "",
+    title: "Wild Code School Biarritz Website",
+    subtitle: "Projet de Formation en Groupe",
+  },
+];
+
+//modifier les descriptions des projets
+//Prendre toutes les images les ajouter au dossier et mettre les chemins relatifs
+export const ProjectDetails = [
+  {
+    id: 1,
+    Title: "Bionbe",
+    Subtitle: "Projet de Stage",
+    Description:
+      "Ce projet à été réalisé au cour de mon stage chez Pic Digital en collaboration avec mon tuteur. C'est un projet que j'ai démarré de zéro. Je devais améliorer la modale de recherche pour le site Bionbe afin que les utilisateurs puissent filtrer leurs recherches facilement et ainsi trouver des activités, des évènements, des clubs et des personnes autour d'eux.",
+    TechnologiesTitle: "Les technologies utilisées",
+    Technologies: ["Vite", "Typescript", "Sass"],
+    Image: "Chemin_vers_une_image",
+    ImageGallery: ["Chemin_vers_image1", "Chemin_vers_image2"],
+  },
+  {
+    id: 2,
+    Title: "Aelys",
+    Subtitle: "Projet de Stage",
+    Description:
+      "Ce projet à été réalisé au cour de mon stage chez Pic Digital en collaboration avec mon tuteur. C'est un projet que j'ai démarré de zéro. Je devais améliorer la modale de recherche pour le site Bionbe afin que les utilisateurs puissent filtrer leurs recherches facilement et ainsi trouver des activités, des évènements, des clubs et des personnes autour d'eux.",
+    TechnologiesTitle: "Les technologies utilisées",
+    Technologies: ["Vite", "Typescript", "Sass"],
+    Image: "Chemin_vers_une_image",
+    ImageGallery: ["Chemin_vers_image1", "Chemin_vers_image2"],
+  },
+  {
+    id: 3,
+    Title: "Bijoux de Soins",
+    Subtitle: "Projet en Autonomie",
+    Description:
+      "Ce projet à été réalisé au cour de mon stage chez Pic Digital en collaboration avec mon tuteur. C'est un projet que j'ai démarré de zéro. Je devais améliorer la modale de recherche pour le site Bionbe afin que les utilisateurs puissent filtrer leurs recherches facilement et ainsi trouver des activités, des évènements, des clubs et des personnes autour d'eux.",
+    TechnologiesTitle: "Les technologies utilisées",
+    Technologies: ["Vite", "Typescript", "Sass"],
+    Image: "Chemin_vers_une_image",
+    ImageGallery: ["Chemin_vers_image1", "Chemin_vers_image2"],
+  },
+
+  {
+    id: 4,
+    Title: "Externatic",
+    Subtitle: "Projet de Formartion en Groupe",
+    Description:
+      "Ce projet à été réalisé au cour de mon stage chez Pic Digital en collaboration avec mon tuteur. C'est un projet que j'ai démarré de zéro. Je devais améliorer la modale de recherche pour le site Bionbe afin que les utilisateurs puissent filtrer leurs recherches facilement et ainsi trouver des activités, des évènements, des clubs et des personnes autour d'eux.",
+    TechnologiesTitle: "Les technologies utilisées",
+    Technologies: ["Vite", "Typescript", "Sass"],
+    Image: "Chemin_vers_une_image",
+    ImageGallery: ["Chemin_vers_image1", "Chemin_vers_image2"],
+  },
+  {
+    id: 5,
+    Title: "EcoTripr",
+    Subtitle: "Projet de Formartion en Groupe",
+    Description:
+      "Ce projet à été réalisé au cour de mon stage chez Pic Digital en collaboration avec mon tuteur. C'est un projet que j'ai démarré de zéro. Je devais améliorer la modale de recherche pour le site Bionbe afin que les utilisateurs puissent filtrer leurs recherches facilement et ainsi trouver des activités, des évènements, des clubs et des personnes autour d'eux.",
+    TechnologiesTitle: "Les technologies utilisées",
+    Technologies: ["Vite", "Typescript", "Sass"],
+    Image: "Chemin_vers_une_image",
+    ImageGallery: ["Chemin_vers_image1", "Chemin_vers_image2"],
+  },
+  {
+    id: 6,
+    Title: "FleetGo",
+    Subtitle: "Projet de Formartion en Groupe",
+    Description:
+      "Ce projet à été réalisé au cour de mon stage chez Pic Digital en collaboration avec mon tuteur. C'est un projet que j'ai démarré de zéro. Je devais améliorer la modale de recherche pour le site Bionbe afin que les utilisateurs puissent filtrer leurs recherches facilement et ainsi trouver des activités, des évènements, des clubs et des personnes autour d'eux.",
+    TechnologiesTitle: "Les technologies utilisées",
+    Technologies: ["Vite", "Typescript", "Sass"],
+    Image: "Chemin_vers_une_image",
+    ImageGallery: ["Chemin_vers_image1", "Chemin_vers_image2"],
+  },
+  {
+    id: 7,
+    Title: "WildFit",
+    Subtitle: "Projet de Formartion en Groupe",
+    Description:
+      "Ce projet à été réalisé au cour de mon stage chez Pic Digital en collaboration avec mon tuteur. C'est un projet que j'ai démarré de zéro. Je devais améliorer la modale de recherche pour le site Bionbe afin que les utilisateurs puissent filtrer leurs recherches facilement et ainsi trouver des activités, des évènements, des clubs et des personnes autour d'eux.",
+    TechnologiesTitle: "Les technologies utilisées",
+    Technologies: ["Vite", "Typescript", "Sass"],
+    Image: "Chemin_vers_une_image",
+    ImageGallery: ["Chemin_vers_image1", "Chemin_vers_image2"],
+  },
+  {
+    id: 8,
+    Title: "Wild Code School Biarritz Website",
+    Subtitle: "Projet de Formation en Groupe",
+    Description:
+      "Ce projet était le premier de ma formation. Il nous a permis de nous familiariser avec les langages de base du développement web (HTML, CSS, JavaScript). Nous l’avons réalisé par groupe de 4 et avons chacun travaillé sur une page différente du site. Pour ma part j’ai réalisé la page contact, avec la création d’une navigation responsive, un formulaire de contact, comprenant quelques animations, une validation du message à la soumission du formulaire, une carte indiquant l’emplacement de l’école, ainsi qu’un Footer avec l’accès aux différents réseaux sociaux de l’école.",
+    TechnologiesTitle: "Les technologies utilisées",
+    Technologies: ["HTML", "CSS", "JavaScript"],
+    Image: "Chemin_vers_une_image",
+    ImageGallery: [
+      "Chemin_vers_image1",
+      "Chemin_vers_image2",
+      "Chemin_vers_image3",
+    ],
+  },
+];
