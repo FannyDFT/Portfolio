6db13726903e397b21b5bb614c221e6f044cdeb7
Fanny D'Erfurth <fannyd.erfurth@yahoo.fr>
2023-10-09 16:47:13 +0200

    improvement folders and css

---
+++ portfolio/src/components/Projects/content/MyProjects.tsx
@@ -0,0 +1,31 @@
+"use client";
+import React from "react";
+import Header from "../header/Header";
+import "./_myprojects.scss";
+import ProjectCard from "./ProjectCard";
+import { Project } from "@/type";
+import { Projects } from "@/datas/datas";
+import "./_projectslist.scss";
+
+function MyProjects() {
+  const firstThreeProjects: Project[] = Projects.slice(0, 3);
+
+  return (
+    <div className="box">
+      <Header />
+      <div className="container_projects">
+        {firstThreeProjects &&
+          firstThreeProjects.map((project) => (
+            <ProjectCard
+              key={project.id}
+              image={project.image}
+              title={project.title}
+              subtitle={project.subtitle}
+            />
+          ))}
+      </div>
+    </div>
+  );
+}
+
+export default MyProjects;
