c0bcea8bdd8ae5c71d443cf7de26643c0e168fd5
Fanny D'Erfurth <fannyd.erfurth@yahoo.fr>
2023-10-20 10:28:28 +0200

    change import fonts

--- src/components/Profil/Profil.tsx
+++ src/components/Profil/Profil.tsx
@@ -1,5 +1,11 @@
 import React from "react";
 import "./_profil.scss";
+
+import { Raleway, Yeseva_One } from "next/font/google";
+
+const raleway = Raleway({ subsets: ["latin"] });
+const yeseva = Yeseva_One({ weight: "400", subsets: ["latin"] });
+
 interface ProfilProps {
   title: string;
   subtitle: string;
@@ -10,10 +16,10 @@ interface ProfilProps {
 function Profil({ title, subtitle, firstText, secondText }: ProfilProps) {
   return (
     <div className="profil">
-      <h1>{title}</h1>
-      <h2>{subtitle}</h2>
-      <p>{firstText}</p>
-      <p>{secondText}</p>
+      <h1 className={yeseva.className}>{title}</h1>
+      <h2 className={raleway.className}>{subtitle}</h2>
+      <p className={raleway.className}>{firstText}</p>
+      <p className={raleway.className}>{secondText}</p>
     </div>
   );
 }
